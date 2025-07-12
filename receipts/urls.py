8b9d066f996acdbf0da0bb0ba21427267b260772
Ambady <ambadydileep69@gmail.com>
2025-07-13 00:02:27 +0530

    adding a customer and editing a customer

--- receipts/urls.py
+++ receipts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('api/test/',views.test_view),
     path('', views.home_view),
+    path('customers/', views.customers_list, name='customer_list'),
+    path('customers/add/',views.add_customer, name='add_customer'),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Customer,Receipt,Item 
+
 # Create your views here.
 
 def test_view(request):
@@ -7,3 +11,18 @@ def test_view(request):
 
 def home_view(request):
     return HttpResponse("Welcome to the Quick Receipt Generator App!")
+
+def customers_list(request):
+    customers = Customer.objects.order_by('-created_at')
+    context = {'customers':customers}
+    return render(request,'receipts/customers_list.html',context)
+
+def add_customer(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+
+        Customer.objects.create(name=name,email=email,phone=phone)
+        return redirect('customers_list')
+    return render(request,'receipts/add_customer.html')
