c687382b3bf8b3f6f8acb987bb6f9d7a314da55c
Ambady <ambadydileep69@gmail.com>
2025-07-12 20:39:01 +0530

    models setup and registed in the admin.py

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Receipt, Item
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Receipt)
+admin.site.register(Item)
+
--- receipts/models.py
+++ receipts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(blank=True,null=True)
+    phone = models.CharField(max_length=15,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Receipt(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Item(models.Model):
+    receipt = models.ForeignKey(Receipt,on_delete=models.CASCADE,related_name='items')
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity}*{self.name} - ₹{self.price}"
+    
+
+
